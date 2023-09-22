63ceef41d8fca72fa8ec5ff9a3ef0c4100426777
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-09-22 15:33:17 -0300

    iniciando integração do firebase

---
+++ taugor/src/config/configuraFirebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBhs9PQnte26EUwf5kwSDKMoPL3QzwPv0U",
+  authDomain: "cadastro-taugor.firebaseapp.com",
+  projectId: "cadastro-taugor",
+  storageBucket: "cadastro-taugor.appspot.com",
+  messagingSenderId: "308655623057",
+  appId: "1:308655623057:web:74460aff19b4c43da12830",
+  measurementId: "G-3HFNMRRRHE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
