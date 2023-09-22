ad60231eafb737abd44ee03fe90d2a0cf09d9c87
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-09-22 15:10:40 -0300

    intalando dependencias

---
+++ taugor/src/main.tsx
@@ -0,0 +1,9 @@
+
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Login from './pages/Login'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  
+  <Login />
+)
---
+++ taugor/src/pages/Login/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.scss"
+
+function Login() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className={styles.geral} >
+      <h2 >Login</h2>
+     </div>
+    </>
+  )
+}
+
+export default Login
