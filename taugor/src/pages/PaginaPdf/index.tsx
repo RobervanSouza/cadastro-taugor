6df5c313a044cdc8e7cbcbed3d552c4845ea2064
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-09-26 12:36:51 -0300

    arualização baixar

--- taugor/src/pages/PaginaPdf/index.tsx
+++ taugor/src/pages/PaginaPdf/index.tsx
@@ -40,65 +40,68 @@ function PaginaPDF() {
 
   return (
     <div className={styles.geral}>
-      <div className="pdf" >
-      <h2 >Dados do Usuário</h2>
-        
-      <ul>
-        <li>
-          <strong>Nome:</strong> {usuario.name}
-        </li>
-        <li>
-          <strong>Sexo:</strong> {usuario.sexo}
-        </li>
-        <li>
-          <strong>Endereço:</strong> {usuario.endereco}
-        </li>
-        <li>
-          <strong>Contato:</strong> {usuario.telefone}
-        </li>
-        <li>
-          <strong>Data de Nascimento:</strong> {usuario.nascimento}
-        </li>
-        <li>
-          <strong>Setor:</strong> {usuario.setor}
-        </li>
-        <li>
-          <strong>Salário:</strong> {usuario.salario}
-        </li>
-        <li>
-          <strong>Data de Admissão:</strong> {usuario.admisao}
-        </li>
-        <li>
-          <strong>Cargo Atual:</strong> {usuario.cargo}
-        </li>
-      </ul>
-      {usuario.cargoHistorico && usuario.cargoHistorico.length > 0 && (
-        <div>
-          <h2>Histórico de Cargo</h2>
-          <ul>
-            {usuario.cargoHistorico.map((cargo: string, index: number) => (
-              <li key={index}>
-                {index + 1}° Cargo: {cargo}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <div className="pdf">
+        <h2>Dados do Usuário</h2>
+
+        <ul>
+          <li>
+            <strong>Nome:</strong> {usuario.name}
+          </li>
+          <li>
+            <strong>Sexo:</strong> {usuario.sexo}
+          </li>
+          <li>
+            <strong>Endereço:</strong> {usuario.endereco}
+          </li>
+          <li>
+            <strong>Contato:</strong> {usuario.telefone}
+          </li>
+          <li>
+            <strong>Data de Nascimento:</strong> {usuario.nascimento}
+          </li>
+          <li>
+            <strong>Setor:</strong> {usuario.setor}
+          </li>
+          <li>
+            <strong>Salário:</strong> {usuario.salario}
+          </li>
+          <li>
+            <strong>Data de Admissão:</strong> {usuario.admisao}
+          </li>
+          <li>
+            <strong>Cargo Atual:</strong> {usuario.cargo}
+          </li>
+        </ul>
+        {usuario.cargoHistorico && usuario.cargoHistorico.length > 0 && (
+          <div>
+            <h2>Histórico de Cargo</h2>
+            <ul>
+              {usuario.cargoHistorico.map((cargo: string, index: number) => (
+                <li key={index}>
+                  {index + 1}° Cargo: {cargo}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <Button type="button" variant="contained" color="primary">
         <Link to="/home" style={{ textDecoration: "none", color: "white" }}>
           Voltar para home
         </Link>
       </Button>
-      <button onClick={baixarPDF}
-      disabled={!leitura === false}
-      >
-        {leitura?(
-          <span>Downloading</span>
-          ):(
-            <span>douload</span>
-            )}
-      </button>
+
+      <Button
+        type="button"
+        variant="contained"
+        color="primary"
+        onClick={baixarPDF}
+        disabled={!leitura === false}
+        >
+        {" "}
+        {leitura ? <span>downloading...</span> : <span>dowload</span>}{" "}
+      </Button>
+      
     </div>
   );
 }
