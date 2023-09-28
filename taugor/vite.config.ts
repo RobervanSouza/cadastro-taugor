3c92203015536ec366f1d181c79d8516da365a90
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-09-28 18:41:34 -0300

    atualização port

--- taugor/vite.config.ts
+++ taugor/vite.config.ts
@@ -6,9 +6,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5137,
-  },
+  
   build: {
     target: "server",
     outDir: "dist",
