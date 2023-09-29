ed3a2e4c0431eba4bf5ee0251948b3a85ffe0a21
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-09-28 21:56:37 -0300

    colocando reload

--- taugor/src/pages/Home/index.tsx
+++ taugor/src/pages/Home/index.tsx
@@ -3,18 +3,21 @@ import { ref, get, child, remove } from "firebase/database";
 import {  database } from "../../config/configuraFirebase";
 import { UserType } from "../../types/userTypes";
 import UserCard from "../../components/cardHome/card";
-
+import Reload from "../../assets/reload.gif"
 
 import styles from "./styles.module.scss";
 import Header from "../../components/header/header";
 
 function ListarUsuarios() {
   const [usuarios, setUsuarios] = useState<UserType[]>([]);
+  const[isLoading, setIsLoading] = useState(true);
+  const [isLoaded, setIsLoaded] = useState(false);
+  
+useEffect(() => {
+  const usuariosRef = ref(database, "users");
 
-  useEffect(() => {
-    const usuariosRef = ref(database, "users");
-
-    get(child(usuariosRef, "/")).then((snapshot) => {
+  get(child(usuariosRef, "/"))
+    .then((snapshot) => {
       if (snapshot.exists()) {
         const usuariosArray: UserType[] = [];
         snapshot.forEach((childSnapshot) => {
@@ -22,11 +25,13 @@ function ListarUsuarios() {
           usuariosArray.push(usuario);
         });
         setUsuarios(usuariosArray);
+        setIsLoaded(true); // Defina como verdadeiro quando os dados forem carregados com sucesso
       }
+    })
+    .finally(() => {
+      setIsLoading(false); // Defina como falso após o carregamento, seja bem-sucedido ou não
     });
-  }, []);
-
-  
+}, []);
 
   const onUpdateUser = (updatedUser: UserType) => {
     setUsuarios((prevUsers) =>
@@ -39,10 +44,10 @@ function ListarUsuarios() {
       // Crie uma referência para o usuário que você deseja excluir no Firebase
       const usuarioRef = ref(database, `users/${userId}`);
 
-      // Use a função "remove" do Firebase para excluir o nó do usuário
+      
       await remove(usuarioRef);
 
-      // Atualize a lista de usuários após a exclusão
+      
       setUsuarios((prevUsers) =>
         prevUsers.filter((user) => user.id !== userId)
       );
@@ -51,24 +56,37 @@ function ListarUsuarios() {
     }
   };
 
+  const reloadData = () => {
+    setIsLoading(true);
+
+    // Recarregue os dados aqui, por exemplo, refaça a consulta ao Firebase
+
+    // Após a conclusão do carregamento, defina isLoading como falso
+    setIsLoading(false);
+  };
+
   return (
     <>
       <header>
         <Header />
       </header>
       <div className={styles.geral}>
-      
-        <h1>Lista de funcionarios</h1>
-        <div className={styles.userList}>
-          {usuarios.map((usuario) => (
-            <UserCard
-              key={usuario.id}
-              usuario={usuario}
-              onUpdateUser={onUpdateUser}
-              onDeleteUser={deleteUserFromFirebase}
-            />
-          ))}
-        </div>
+        <h1>Lista de funcionários</h1>
+        {isLoading ? (
+          <img src={Reload} alt="Carregando..." />
+        ) : (
+          <div className={styles.userList}>
+            {usuarios.map((usuario) => (
+              <UserCard
+                key={usuario.id}
+                usuario={usuario}
+                onUpdateUser={onUpdateUser}
+                onDeleteUser={deleteUserFromFirebase}
+              />
+            ))}
+          </div>
+        )}
+        {isLoaded && <button onClick={reloadData}>Recarregar</button>}
       </div>
     </>
   );
