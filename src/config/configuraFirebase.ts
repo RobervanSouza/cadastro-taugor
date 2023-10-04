bc382488ac2bfc78c0e3c33d658d381c04cce4e1
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-10-04 13:06:30 -0300

    verificando variavel

--- src/config/configuraFirebase.ts
+++ src/config/configuraFirebase.ts
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { browserLocalPersistence, getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-import { AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID, API_KEY } from "../env";
+//import { AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID, API_KEY } from "../env";
 
 
 
@@ -11,36 +11,29 @@ import { AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, M
 
  
 
-const firebaseConfig = {
-  authDomain:  AUTH_DOMAIN ,
-  //projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID,
-  projectId: "cadastro-taugor",
-   apiKey: API_KEY,
-};
-
 // const firebaseConfig = {
-//  apiKey: "AIzaSyBhs9PQnte26EUwf5kwSDKMoPL3QzwPv0U",
-//   authDomain: "cadastro-taugor.firebaseapp.com",
-//   storageBucket: "cadastro - taugor.appspot.com",
-//   messagingSenderId: "308655623057",
-//   appId: "1:308655623057:web:74460aff19b4c43da12830",
-//   measurementId: "G-3HFNMRRRHE",
+//   authDomain:  AUTH_DOMAIN ,
+//   //projectId: PROJECT_ID,
+//   storageBucket: STORAGE_BUCKET,
+//   messagingSenderId: MESSAGING_SENDER_ID,
+//   appId: APP_ID,
+//   measurementId: MEASUREMENT_ID,
+//   projectId: "cadastro-taugor",
+//    apiKey: API_KEY,
 // };
 
+const firebaseConfig = {
+ apiKey: "AIzaSyBhs9PQnte26EUwf5kwSDKMoPL3QzwPv0U",
+  authDomain: "cadastro-taugor.firebaseapp.com",
+  storageBucket: "cadastro - taugor.appspot.com",
+  messagingSenderId: "308655623057",
+  appId: "1:308655623057:web:74460aff19b4c43da12830",
+  measurementId: "G-3HFNMRRRHE",
+};
 
 
-// VITE_REACT_APP_API_KEY = "AIzaSyBhs9PQnte26EUwf5kwSDKMoPL3QzwPv0U";
-// VITE_REACT_APP_AUTH_DOMAIN = "cadastro-taugor.firebaseapp.com";
-// VITE_REACT_APP_PROJECT_ID = "cadastro-taugor";
-// VITE_REACT_APP_STORAGE_BUCKET = "cadastro-taugor.appspot.com";
-// VITE_REACT_APP_MESSAGING_SENDER_ID = "308655623057";
-// VITE_REACT_APP_APP_ID = "1:308655623057:web:74460aff19b4c43da12830";
-// VITE_REACT_APP_MEASUREMENT_ID = "G-3HFNMRRRHE";
-// VITE_REACT_APP_ADMIN_EMAIL = "taugor@getnet.com";
+
+
 const app = initializeApp(firebaseConfig);
 
   const database = getDatabase(app)
